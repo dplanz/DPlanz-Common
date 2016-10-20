30e4b12cf0b188330261e7f1d2d9d1bb406bc31d
Derrick Planz <derrick.planz@gmail.com>
2016-10-20 15:18:05 -0400

    Added Images & DataSets

---
+++ dplanz-common/dplanz-common/DataSets.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CommonLibrary
+{
+    class DataSets
+    {
+        public static DataTable ConvertListToDataTable<T>(IEnumerable<T> list)
+        {
+            var type = typeof(T);
+            var properties = type.GetProperties();
+
+            var dataTable = new DataTable();
+            foreach (var info in properties)
+            {
+                dataTable.Columns.Add(
+                    Nullable.GetUnderlyingType(info.PropertyType) != null
+                    ? new DataColumn(info.Name, Nullable.GetUnderlyingType(info.PropertyType))
+                    : new DataColumn(info.Name, info.PropertyType));
+            }
+
+            foreach (var entity in list)
+            {
+                var values = new object[properties.Length];
+                for (var i = 0; i < properties.Length; i++)
+                {
+                    values[i] = properties[i].GetValue(entity);
+                }
+
+                dataTable.Rows.Add(values);
+            }
+
+            return dataTable;
+        }
+
+        public static class Medians
+        {
+            public static decimal GetMedian(List<int> source)
+            {
+                // Create a copy of the input, and sort the copy
+                var temp = source.ToArray();
+                Array.Sort(temp);
+
+                var count = temp.Length;
+                if (count == 0)
+                {
+                    throw new InvalidOperationException("Empty collection");
+                }
+                if (count%2 != 0) return temp[count/2];
+                // count is even, average two middle elements
+                var a = temp[count / 2 - 1];
+                var b = temp[count / 2];
+                return (a + b) / 2m;
+                // count is odd, return the middle element
+            }
+            public static decimal GetQuad1(List<int> source)
+            {
+                // Create a copy of the input, and sort the copy
+                var temp = source.ToArray();
+                Array.Sort(temp);
+
+                var count = temp.Length;
+                if (count == 0)
+                {
+                    throw new InvalidOperationException("Empty collection");
+                }
+                if (count % 4 == 0)
+                {
+                    // count is even, average two middle elements
+                    var a = temp[count / 4 - 1];
+                    var b = temp[count / 4];
+                    return (a + b) / 2m;
+
+                }
+                // count is odd, return the middle element
+                return temp[count / 4];
+            }
+            public static decimal GetQuad3(List<int> source)
+            {
+                // Create a copy of the input, and sort the copy
+                var temp = source.ToArray();
+                Array.Sort(temp);
+
+                var count = temp.Length;
+                if (count == 0)
+                {
+                    throw new InvalidOperationException("Empty collection");
+                }
+                if (count % 4 == 0)
+                {
+                    // count is even, average two middle elements
+                    var a = temp[3 * count / 4 - 1];
+                    var b = temp[3 * count / 4];
+                    return (a + b) / 2m;
+
+                }
+                // count is odd, return the middle element
+                return temp[3 * count / 4];
+            }
+        }
+    }
+}
---
+++ dplanz-common/dplanz-common/Images.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace CommonLibrary
+{
+    public class Images
+    {
+        public static byte[] ImageToByteArray(Image imageIn)
+        {
+            var ms = new MemoryStream();
+            imageIn.Save(ms, ImageFormat.Jpeg);
+            return ms.ToArray();
+        }
+        public static string ImageToBase64(Image imageIn)
+        {
+            var ms = new MemoryStream();
+            imageIn.Save(ms, ImageFormat.Jpeg);
+            var imageBytes = ms.ToArray();
+            return Convert.ToBase64String(imageBytes);
+        }
+        public static Image ByteArrayToImage(byte[] byteArrayIn)
+        {
+            var ms = new MemoryStream(byteArrayIn);
+            var returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+
+        public static Image ResizeImage(Image img, int maxWidth, int maxHeight)
+        {
+            double w = 0;
+            double h = 0;
+
+            if (img.Width > img.Height)
+            {
+                w = 1;
+                h = img.Height / (double)img.Width;
+            }
+            else if (img.Width < img.Height)
+            {
+                w = img.Width / (double)img.Height;
+                h = 1;
+            }
+            else if (img.Width == img.Height)
+            {
+                w = 1;
+                h = 1;
+            }
+
+            return new Bitmap(img, new Size((int)Math.Round(maxWidth * w), (int)Math.Round(maxHeight * h)));
+        }
+    }
+}
